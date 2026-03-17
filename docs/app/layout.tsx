1c294b2fe5e64e7391bd48b32670b3db8bb12742
QUDDUS <qudduslarek@proton.me>
2026-03-17 14:19:50 +0500

    update UI and docs

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -2,13 +2,13 @@ import { Inter, Google_Sans } from 'next/font/google';
 import { Provider } from '@/components/provider';
 import './global.css';
 
-const inter = Google_Sans({
+const GoogleSans = Google_Sans({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={GoogleSans.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <Provider>{children}</Provider>
       </body>
