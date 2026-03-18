126dd17dec6d1c25565a1233d5422ffa285d64c1
QUDDUS <qudduslarek@proton.me>
2026-03-18 07:35:30 +0500

    add OGs

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -10,6 +10,28 @@ const GoogleSans = Google_Sans({
 export const metadata: Metadata = {
   title: 'leehooks - Advanced React Hooks Library',
   description: 'The advanced React hook library built for React 19 by the lixril team.',
+  openGraph: {
+    title: 'leehooks - Advanced React Hooks Library',
+    description: 'The advanced React hook library built for React 19 by the lixril team.',
+    images: [
+      {
+        url: 'https://github.com/user-attachments/assets/8122999c-d513-4a25-9ead-400d699293d7',  // Place a 1200x630 PNG/JPG in public/
+        width: 1200,
+        height: 630,
+        alt: 'leehooks',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'leehooks - Advanced React Hooks Library',
+    description: 'The advanced React hook library built for React 19 by the lixril team.',
+    images: ['https://github.com/user-attachments/assets/8122999c-d513-4a25-9ead-400d699293d7'],
+  },
+};
+
+type LayoutProps<T extends string> = {
+  children: React.ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps<'/'>) {
