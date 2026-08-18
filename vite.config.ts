cb0288ceb4810cfc4e00820b0f61a0023b5c36cc
QUDDUS <qudduslarek@proton.me>
2026-08-18 17:38:57 +0500

    update to simple site

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import tailwindcss from '@tailwindcss/vite'; // if using Tailwind v4
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
     react(),
-    // tailwindcss(), 
-  ],
-  build: {
-    lib: {
-      entry: 'src/index.ts', // your main export file
-      name: 'Chart-witgets',
-      formats: ['es', 'cjs'],
-      fileName: (format) =>
-        format === 'es' ? 'index.js' : 'index.cjs',
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-    },
-    sourcemap: true,
-    minify: false, // usually false for libraries
-  },
+    tailwindcss(),
+  ]
 });
